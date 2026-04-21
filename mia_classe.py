0a2cb2116940d9a1865a5766555e7b4c0a15977a
feliciaproietti <proietti.felicia06@gmail.com>
2026-04-21 15:50:36 +0000

    lezione 21 aprile

---
+++ mia_classe.py
@@ -0,0 +1,39 @@
+#
+
+class Punto:
+    """Una classe per rappresentare e operere con una coppia di coordinate x,y"""
+    
+    @classmethod
+    def origin(cls) :
+        """Metodo della classe che imposta i valori x=0, y=0"""
+        return cls(0, 0)   # cls contiene tutte le informazioni sulla classe
+                
+    def __init__(self, x, y):
+        """Inizializza le coordinate del punto"""
+        self.x = x
+        self.y = y
+    def distanza_da_punto(self, altro_punto):
+        #distanza = ((x2 - x1)**2 + (y2-y1)**2)**0.5
+        dist = ((altro_punto.x - self.x)**2 + (altro_punto.y - self.y)**2)**0.5
+        return dist
+    def __str__(self):
+        testo = f"E' un punto di coordinata x={self.x} e y = {self.y}"
+        return testo
+
+class PuntoMassa(Punto):
+    #...
+    g = 9.81
+
+    def __init__(self, x=0, y=0, m=0):
+        """Sovrascrive l'__init__ di Punto per tenere conto della massa"""
+        Punto.__init__(self, x, y)  # Per le coordinate, uso l'__init__ di 'Punto'
+        self.m = m            # Aggiungo il nuovo attributo: massa
+
+    def peso(self):
+        """Restitusce il peso dell'oggetto"""
+        return self.m*self.g
+        
+
+
+
+        
